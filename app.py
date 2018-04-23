52edd07f74bfae55c91a255ef906579bf095ed62
Yanming Lai <yanminglai91@gwu.edu>
2018-04-23 06:37:41 -0400

    initial commit

---
+++ app.py
@@ -0,0 +1,100 @@
+from flask import Flask, request
+from redis import Redis, RedisError
+import os
+import socket
+from PIL import Image, ImageFilter
+from cassandra.cluster import Cluster
+import time
+import sys
+import tensorflow as tf
+
+# Connect to Redis
+redis = Redis(host="redis", db=0, socket_connect_timeout=2, socket_timeout=2)
+
+app = Flask(__name__)
+
+### Model Setup
+x = tf.placeholder(tf.float32, [None, 784])
+W = tf.Variable(tf.zeros([784, 10]))
+b = tf.Variable(tf.zeros([10]))
+y = tf.nn.softmax(tf.matmul(x, W) + b)
+init = tf.global_variables_initializer()
+saver = tf.train.Saver()
+sess = tf.Session()
+sess.run(init)
+saver.restore(sess, "mnist_project/models")
+##############
+
+### Connect to the cassandra database
+cluster = Cluster(contact_points=["172.17.0.2"],port=9042)
+session = cluster.connect()
+### Create KEYSPACE and TABLE if not exist
+session.execute("create KEYSPACE if not exists mnist_database WITH replication = {'class':'SimpleStrategy', 'replication_factor': 2};")
+session.execute("use mnist_database")
+session.execute("create table if not exists mnist(id uuid, digits int, image_name text, upload_time timestamp, primary key(id));")
+#############
+
+@app.route("/prediction", methods=['GET','POST'])
+def predictint():
+    imname = request.files["file"]   #Input
+    file_name = request.files["file"].filename
+    imvalu = prepareImage(imname)
+    prediction = tf.argmax(y,1)
+    pred = prediction.eval(feed_dict={x: [imvalu]}, session=sess)
+    #grab time
+    uploadtime=time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(time.time()))
+    #insert data to cassandra
+    session.execute("INSERT INTO mnist(id, digits, image_name, upload_time) values(uuid(), %s, %s, %s)",[int(str(pred[0])), file_name, uploadtime])
+    return "The number upload is: %s  (91%) " % str(pred[0])
+
+def prepareImage(i):
+    im = Image.open(i).convert('L')
+    width = float(im.size[0])
+    height = float(im.size[1])
+    newImage = Image.new('L', (28, 28), (255)) #creates white canvas of 28x28 pixels
+    
+    if width > height: #check which dimension is bigger
+        #Width is bigger. Width becomes 20 pixels.
+        nheight = int(round((20.0/width*height),0)) #resize height according to ratio width
+        if (nheight == 0): #rare case but minimum is 1 pixel
+            nheight = 1
+        # resize and sharpen
+        img = im.resize((20,nheight), Image.ANTIALIAS).filter(ImageFilter.SHARPEN)
+        wtop = int(round(((28 - nheight)/2),0)) #caculate horizontal pozition
+        newImage.paste(img, (4, wtop)) #paste resized image on white canvas
+    else:
+        #Height is bigger. Height becomes 20 pixels. 
+        nwidth = int(round((20.0/height*width),0)) #resize width according to ratio height
+        if (nwidth == 0): #rare case but minimum is 1 pixel
+            nwidth = 1
+         # resize and sharpen
+        img = im.resize((nwidth,20), Image.ANTIALIAS).filter(ImageFilter.SHARPEN)
+        wleft = int(round(((28 - nwidth)/2),0)) #caculate vertical pozition
+        newImage.paste(img, (wleft, 4)) #paste resized image on white canvas
+    
+    tv = list(newImage.getdata()) #get pixel values
+    
+    #normalize pixels to 0 and 1. 0 is pure white, 1 is pure black.
+    tva = [ (255-x)*1.0/255.0 for x in tv] 
+    return tva  # Vector of values
+
+@app.route('/')
+def index():
+    try:
+        visits = redis.incr("counter")
+    except RedisError:
+        visits = "<i>cannot connect to Redis, counter disabled</i>"
+
+    html = '''
+	<!doctype html>
+	<html>
+	<body>
+	<form action='/prediction' method='post' enctype='multipart/form-data'>
+  		<input type='file' name='file'>
+	<input type='submit' value='Upload'>
+	</form>
+	'''   
+    return html.format(name=os.getenv("NAME", "MNIST"), hostname=socket.gethostname(), visits=visits) 
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0',port=80)
---
+++ get_model.py
@@ -0,0 +1,31 @@
+import tensorflow as tf
+from tensorflow.examples.tutorials.mnist import input_data
+
+## This is where the data gets imported
+mnist = input_data.read_data_sets("MNIST_data/", one_hot=True)
+
+# Model Creation - Placeholders & Variables
+x = tf.placeholder(tf.float32, [None, 784])
+W = tf.Variable(tf.zeros([784, 10]))
+b = tf.Variable(tf.zeros([10]))
+y = tf.nn.softmax(tf.matmul(x, W) + b)
+
+# Defining the loss and optimizations
+y_ = tf.placeholder(tf.float32, [None, 10])
+cross_entropy = -tf.reduce_sum(y_ * tf.log(y))
+train_step = tf.train.GradientDescentOptimizer(0.01).minimize(cross_entropy)
+
+init_op = tf.global_variables_initializer()
+saver = tf.train.Saver()
+
+# The model is saved to disk as a model.ckpt file
+
+# Initializing the Session
+with tf.Session() as sess:
+    sess.run(init_op)
+    for i in range(1000):
+        batch_xs, batch_ys = mnist.train.next_batch(100)
+        sess.run(train_step, feed_dict={x: batch_xs, y_: batch_ys})
+
+    save_path = saver.save(sess, "/mnist_project/models")
+print ("Model saved in file: ", save_path)
